cc1b171d3fb4ad599b0cb0d5fbf8a0f3e0999571
Per Steen Olsen <persteen22@hotmail.com>
2021-12-13 20:04:10 +0100

    React client working with a Web API sequelize MySQL serving as a membership system

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render } from 'react-dom';
+
+import { history } from './_helpers';
+import { accountService } from './_services';
+import { App } from './app';
+
+// Note: Using an Alias in Webpack
+import 'styles/index.scss';
+
+// Note: Give a wake up ping to the server ( at Azure ) from the idle mode and get the response value "hello" in x.message !
+ accountService.pingServer()
+ .then( pong => {
+	  
+	  // Take a look at the Crome Developer Console 
+	  console.log( 'INDEX => Pong from the Node Server: ' + pong.message );
+	 });
+
+// Starting the App  !
+startApp(); 
+
+
+function startApp() { 
+    render(
+        <Router history={history}>
+            <App />
+        </Router>,
+        document.getElementById('app')
+    );
+}
+
+// Note: Before npm run build the statement module.hot.accept(); could / should to be disabled / comment out !!!
+// In Webpck HotModuleReplacementPlugin() is used to set hot to true. 
+// This way the browser dont need to reload the entire page when changing  file !
+// Note: Needed here - in contrast to Vue.js  !!
+ if (module.hot) {
+    module.hot.accept();
+ }
+
+
